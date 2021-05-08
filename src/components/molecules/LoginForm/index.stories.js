4daedd007a88b2a4b88700dbea7cb22c44d01d15
ha-young <hayeong28@naver.com>
2021-05-08 20:49:13 +0900

    [ADD] Molecules - LoginForm

--- src/components/molecules/LoginForm/index.stories.js
+++ src/components/molecules/LoginForm/index.stories.js
@@ -1,35 +1,12 @@
-// import { storiesOf } from "@storybook/react";
-// import { LoginForm } from "components";
-// import React from "react";
+import { storiesOf } from "@storybook/react";
+import React from "react";
 
-// storiesOf("LoginForm", module)
-//   .add("default", () => (
-//     <LoginForm title="ARc">
-//       Ullamco duis in labore consectetur ad exercitation esse esse duis mollit
-//       sint.
-//     </LoginForm>
-//   ))
-//   .add("with link", () => (
-//     <LoginForm title="ARc" link="https://github.com/diegohaz/arc">
-//       Ullamco duis in labore consectetur ad exercitation esse esse duis mollit
-//       sint.
-//     </LoginForm>
-//   ))
-//   .add("with icon", () => (
-//     <LoginForm icon="close" title="ARc">
-//       Ullamco duis in labore consectetur ad exercitation esse esse duis mollit
-//       sint.
-//     </LoginForm>
-//   ))
-//   .add("with code", () => (
-//     <LoginForm code="npm run build" title="ARc">
-//       Ullamco duis in labore consectetur ad exercitation esse esse duis mollit
-//       sint.
-//     </LoginForm>
-//   ))
-//   .add("with soon", () => (
-//     <LoginForm soon title="ARc">
-//       Ullamco duis in labore consectetur ad exercitation esse esse duis mollit
-//       sint.
-//     </LoginForm>
-//   ));
+import LoginForm from ".";
+
+storiesOf("LoginForm", module)
+  .add("default", () => (
+    <LoginForm
+      title="Memona"
+      information="당신의 추억의 위치를 기억하세요"
+    />
+  ));
