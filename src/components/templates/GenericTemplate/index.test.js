8571e3c85b39e3a960c549e69fde585846377ae2
ha-young <hayeong28@naver.com>
2021-05-08 21:27:41 +0900

    [ADD] Templates - GenericTemplate

---
+++ src/components/templates/GenericTemplate/index.test.js
@@ -0,0 +1,16 @@
+import { mount, shallow } from "enzyme";
+import React from "react";
+
+import GenericTemplate from ".";
+
+const wrap = (props = {}) =>
+  shallow(<GenericTemplate {...props}>test</GenericTemplate>);
+
+it("mounts", () => {
+  mount(<GenericTemplate>test</GenericTemplate>);
+});
+
+it("renders children when passed in", () => {
+  const wrapper = wrap();
+  expect(wrapper.contains("test")).toBe(true);
+});
