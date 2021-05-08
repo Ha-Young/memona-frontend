7a631a96d5d016d1eb448caee4ec624a3210ab8a
ha-young <hayeong28@naver.com>
2021-05-08 17:15:53 +0900

    [ADD] Atoms - Label

--- src/components/atoms/Input/index.stories.js
+++ src/components/atoms/Input/index.stories.js
@@ -1,33 +1,20 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import Input from '.'
+import { storiesOf } from "@storybook/react";
+import React from "react";
 
-storiesOf('Input', module)
-  .add('default', () => (
-    <Input />
-  ))
-  .add('reverse', () => (
-    <Input reverse />
-  ))
-  .add('height', () => (
-    <Input height={100} />
-  ))
-  .add('invalid', () => (
-    <Input invalid />
-  ))
-  .add('type textarea', () => (
-    <Input type="textarea" />
-  ))
-  .add('type checkbox', () => (
-    <Input type="checkbox" />
-  ))
-  .add('type radio', () => (
-    <Input type="radio" />
-  ))
-  .add('type select', () => (
+import Input from ".";
+
+storiesOf("Input", module)
+  .add("default", () => <Input />)
+  .add("reverse", () => <Input reverse />)
+  .add("height", () => <Input height={100} />)
+  .add("invalid", () => <Input invalid />)
+  .add("type textarea", () => <Input type="textarea" />)
+  .add("type checkbox", () => <Input type="checkbox" />)
+  .add("type radio", () => <Input type="radio" />)
+  .add("type select", () => (
     <Input type="select">
       <option>Option 1</option>
       <option>Option 2</option>
       <option>Option 3</option>
     </Input>
-  ))
+  ));
