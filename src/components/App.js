194032b37ee96ab112967eb835f3e9fd53678d93
ha-young <hayeong28@naver.com>
2021-05-08 14:59:24 +0900

    [ADD] query test code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { Route,Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./themes";
 
+const USER_TEST = gql`
+  query getUsers {
+    userfs {
+      _id
+      email
+      username
+      nickname
+      avatar
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(USER_TEST);
+
+  console.log("loading", loading);
+  console.log("error", error);
+  console.log("data", data);
+
   return (
     <ThemeProvider theme={theme}>
       <Switch>
-        <Route path="/">
-          Memona
-        </Route>
+        <Route path="/"><div>{JSON.stringify(data)}</div></Route>
       </Switch>
     </ThemeProvider>
   );
