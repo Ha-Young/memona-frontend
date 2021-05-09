1644fe0f6f0b1d5c29286e0e3c7bc85a450419e5
ha-young <hayeong28@naver.com>
2021-05-09 23:09:59 +0900

    [ADD] login mutation on loginPage && when success login, save toke

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,4 +1,6 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
+import { Redirect } from "react-router";
 import styled from "styled-components";
 
 import defaultImage from "../../assets/images/landingPhone.png";
@@ -6,6 +8,7 @@ import Img from "../../components/atoms/Img";
 import AddToHomeForm from "../../components/molecules/AddToHomeForm";
 import LoginForm from "../../components/molecules/LoginForm";
 import GenericTemplate from "../../components/templates/GenericTemplate";
+import useToken from "../../hooks/useToken";
 
 const LandingImages = styled.div`
   @media screen and (max-width: 640px) {
@@ -21,23 +24,58 @@ const LandingForms = styled.div`
   height: 580px;
 `;
 
+const LOGIN_USER = gql`
+  mutation LoginMutation(
+    $type: String
+    $token: String
+    $email: String
+    $password: String
+  ) {
+    login(
+      type: $type
+      token: $token
+      email: $email
+      password: $password
+    )
+  }
+`;
+
 const LoginPage = () => {
+  const [login] = useMutation(LOGIN_USER, { onCompleted: handleLoginSuccess, onError: handleLoginFailure });
+  const { token, setToken } = useToken();
+
+  function handleLoginSuccess({ login }) {
+    setToken(login);
+  }
+
+  function handleLogin(loginData) {
+    login({ variables: { ...loginData } });
+  }
+
+  function handleLoginFailure(error) {
+    // todo. error handling
+    console.log(error);
+  }
+
   return (
-    <GenericTemplate>
-      <LandingImages>
-        <Img alt="landingPhone" src={defaultImage} />
-      </LandingImages>
-      <LandingForms>
-        <LoginForm
-          title="Memona"
-          information="당신의 추억의 위치를 기억하세요"
-          height="480px"
-        />
-        <AddToHomeForm
-          height="180px"
-        />
-      </LandingForms>
-    </GenericTemplate>
+    <>
+      <GenericTemplate>
+        <LandingImages>
+          <Img alt="landingPhone" src={defaultImage} />
+        </LandingImages>
+        <LandingForms>
+          <LoginForm
+            title="Memona"
+            information="당신의 추억의 위치를 기억하세요"
+            height="480px"
+            onLogin={handleLogin}
+            onLoginFailure={handleLoginFailure}
+          />
+          <AddToHomeForm height="180px" />
+        </LandingForms>
+      </GenericTemplate>
+      {token && <Redirect to="/" />}
+    </>
   );
 };
 
