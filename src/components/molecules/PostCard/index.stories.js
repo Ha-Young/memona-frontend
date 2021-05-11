ceb70ac2cd8534248bbff651d513d5715fbb504f
ha-young <hayeong28@naver.com>
2021-05-11 17:56:34 +0900

    [FIX] seperate PostCard to PostTitle, PostImg, PostContent

--- src/components/molecules/PostCard/index.stories.js
+++ src/components/molecules/PostCard/index.stories.js
@@ -3,7 +3,15 @@ import React from "react";
 
 import PostCard from ".";
 
+const mockData = {
+  postDate: "2021-05-04",
+  content: "테스트입니다. 테스트 포스트카드입니다.",
+  author: {
+    username: "testuser",
+  },
+};
+
 storiesOf("PostCard", module)
   .add("default", () => (
-    <PostCard postDate="2021-05-04" content="테스트입니다. 테스트 포스트카드입니다."/>
+    <PostCard post={mockData}/>
   ));
