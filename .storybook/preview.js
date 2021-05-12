7d3c25f6ce066ad83b11553936aa69437cefca44
ha-young <hayeong28@naver.com>
2021-05-12 20:17:54 +0900

    [ADD] storybook decorator for theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { MockedProvider } from "@apollo/client/testing";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/components/themes";
+import GlobalStyle from "../src/components/themes/GlobalStyle";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +15,23 @@ export const parameters = {
   },
   apolloClient: {
     MockedProvider,
-    // any props you want to pass to MockedProvider on every story
   },
-}
+};
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_SERVER_URI,
+  cache: new InMemoryCache(),
+});
+
+export const decorators = [
+  (Story) => (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Story />
+        </ThemeProvider>
+      </ApolloProvider>
+    </BrowserRouter>
+  ),
+];
---
+++ .storybook/theme.js
@@ -0,0 +1,4 @@
+import { create } from '@storybook/theming';
+import theme from '../src/components/themes';
+
+export default create(theme);
