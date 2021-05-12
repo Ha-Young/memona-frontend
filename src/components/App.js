a2f791e3079e9ee771706ad5ba2d752415467dd0
ha-young <hayeong28@naver.com>
2021-05-12 21:06:13 +0900

    [ADD] Molecules - SeasonPicker

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 
 import LoginPage from "../pages/LoginPage";
 import MainPage from "../pages/MainPage";
 import AuthRoute from "./AuthRoute";
-import theme from "./themes";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Switch>
-        <AuthRoute path="/" exact>
-          <MainPage />
-        </AuthRoute>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-      </Switch>
-    </ThemeProvider>
+    <Switch>
+      <AuthRoute path="/" exact>
+        <MainPage />
+      </AuthRoute>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+    </Switch>
   );
 }
 
--- src/pages/MainPage/index.stories.js
+++ src/pages/MainPage/index.stories.js
@@ -1,19 +1,8 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { storiesOf } from "@storybook/react";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 
 import MainPage from ".";
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_URI,
-  cache: new InMemoryCache(),
-});
-
 storiesOf("MainPage", module).add("default", () => (
-  <BrowserRouter>
-    <ApolloProvider client={client}>
-      <MainPage />
-    </ApolloProvider>
-  </BrowserRouter>
+  <MainPage />
 ));
