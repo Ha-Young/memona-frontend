d47c2e0e2f93703f435d900d3d2807a8bb4d7bac
ha-young <hayeong28@naver.com>
2021-05-13 23:33:06 +0900

    [DELETE] Camera page because direct AR start for AR authority needs user interactive just like button click

--- src/pages/CameraPage/index.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-
-import CameraPage from ".";
-
-storiesOf("CameraPage", module).add("default", () => (
-  <CameraPage />
-));
