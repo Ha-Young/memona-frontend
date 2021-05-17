d58c608568268e108c2511ac526ad11c5a633d43
ha-young <hayeong28@naver.com>
2021-05-18 02:02:59 +0900

    [ADD] Molecules - Modal using React-Modal node module

---
+++ src/components/molecules/Modal/index.stories.js
@@ -0,0 +1,27 @@
+import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+import Modal from ".";
+
+storiesOf("Modal", module)
+  .add("default", () => (
+    <Modal onClose={() => {console.log("onClose");}} isOpen>
+      Ullamco et reprehenderit magna cillum ullamco consectetur et enim aliqua.
+    </Modal>
+  ))
+  .add("with title", () => (
+    <Modal onClose={() => {console.log("onClose");}} title="Hello" isOpen>
+      Ullamco et reprehenderit magna cillum ullamco consectetur et enim aliqua.
+    </Modal>
+  ))
+  .add("closeable", () => (
+    <Modal onClose={() => {console.log("onClose");}} closeable isOpen>
+      Ullamco et reprehenderit magna cillum ullamco consectetur et enim aliqua.
+    </Modal>
+  ))
+  .add("reverse", () => (
+    <Modal onClose={() => {console.log("onClose");}} reverse isOpen>
+      Ullamco et reprehenderit magna cillum ullamco consectetur et enim aliqua.
+    </Modal>
+  ));
