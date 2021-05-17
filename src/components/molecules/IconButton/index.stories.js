3f839ed0362bb3c5356531ea8a29d3c92752c5fe
ha-young <hayeong28@naver.com>
2021-05-18 02:02:37 +0900

    [ADD] Molecules - IconButton

---
+++ src/components/molecules/IconButton/index.stories.js
@@ -0,0 +1,38 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+import IconButton from ".";
+
+storiesOf("IconButton", module)
+  .add("default", () => <IconButton icon="close">Hello</IconButton>)
+  .add("transparent", () => (
+    <IconButton icon="close" transparent>
+      Hello
+    </IconButton>
+  ))
+  .add("with icon on right", () => (
+    <IconButton icon="close" right>
+      Hello
+    </IconButton>
+  ))
+  .add("responsive", () => (
+    <IconButton icon="close" responsive>
+      Decrease panel width
+    </IconButton>
+  ))
+  .add("responsive with breakpoint", () => (
+    <IconButton icon="close" breakpoint={300} responsive>
+      Decrease panel width to 300
+    </IconButton>
+  ))
+  .add("without text", () => <IconButton icon="close" />)
+  .add("collapsed", () => (
+    <IconButton icon="close" collapsed>
+      Hello
+    </IconButton>
+  ))
+  .add("height", () => (
+    <IconButton icon="close" height={100}>
+      Hello
+    </IconButton>
+  ));
