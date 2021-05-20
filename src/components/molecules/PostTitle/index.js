3fe2c4281de75a54725756d54d37ff878e9becf9
ha-young <hayeong28@naver.com>
2021-05-20 16:44:30 +0900

    [FIX] main page layout - post card viewing infromation

--- src/components/molecules/PostTitle/index.js
+++ src/components/molecules/PostTitle/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { palette } from "styled-theme";
 
+import capitalize from "../../../utils/capitalize";
 import Avatar from "../../atoms/Avatar";
 import Icon from "../../atoms/Icon";
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
+  width: 100%;
 `;
 
 const StyledAvatar = styled(Avatar)`
@@ -22,21 +25,33 @@ const StyledIcon = styled(Icon)`
 `;
 
 const PostTitleContent = styled.span`
-  padding-top: .2rem;
+  display: flex;
+  align-items: center;
+  padding-top: 0.2rem;
   box-sizing: border-box;
+`;
+
+const SeasonInfo = styled.span`
+  font-size: 1.1rem;
+`;
+
+const AreaInfo = styled.span`
+  margin-right: .5rem;
+  font-size: 0.8rem;
 `;
 
 const PostTitle = ({ post, ...props }) => {
   return (
     <Wrapper {...props}>
-      {post.author?.imageUrl ? (
-        <StyledAvatar src={post.author.imageUrl} alt={post.author.username} />
-      ) : (
-        <StyledIcon icon="user" width={32} />
-      )}
       <PostTitleContent>
-        {post.postDate}
+        {post.isAnonymous || post.author?.imageUrl ? (
+          <StyledAvatar src={post.author.imageUrl} alt={post.author.username} />
+        ) : (
+          <StyledIcon icon="user" width={32} />
+        )}
+        <SeasonInfo>{`${post.year} ${capitalize(post.season)}`}</SeasonInfo>
       </PostTitleContent>
+      <AreaInfo>{post.area}</AreaInfo>
     </Wrapper>
   );
 };
